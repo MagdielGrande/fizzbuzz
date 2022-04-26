59f8357123898677691a61fa6b340f91fff2664b
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-26 18:50:30 -0500

    4 Explorer Service punto 6, se refactorizo la logica que se ejecuta sobre la lista de explorers crearon servicios con los 3 metodos estaticos

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
 
 // Part 1 Read json file ===========================
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(explorers)
+
 
 // Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+const explorersInNode = ExplorerService.filterByMission(explorers,"node")
+
 
 // Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+const explorersInNodeToGetUsernames = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+
+const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
