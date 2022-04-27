f0be6fc7061cdac24b3cecfd6f1154d83506bea9
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-27 16:40:13 -0500

    Add pruebas de ExplorerServices

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,24 @@
+const Reader = require("./../../../lib/utils/Reader");
+const ExplorerService = require("./../../../lib/services/ExplorerService")
+const explorers = Reader.readJsonFile("./explorers.json");
+
+describe("Test for Explorerservice", () => {
+
+    test("1. get explorer in node ", () => {
+       const explorersInNode = ExplorerService.filterByMission(explorers,"node")
+       expect(explorersInNode.length).toBe(10)
+       expect.objectContaining({mission: "node"})
+
+    })
+
+    test("2: Get amount of explorers by mission", () => {
+        const explorersGetAmount = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+       expect(explorersGetAmount).toBe(10)
+    })
+
+    test("3. Get Explorers Usernames By Mission", () => {
+        const explorerUserNamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorerUserNamesInNode).toContain("ajolonauta2");
+    });
+
+})
