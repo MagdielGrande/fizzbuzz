570714f3dae6e6ad7228d84310eac6f636bf8d29
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-27 17:16:51 -0500

    Add pruebas de Reader

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,12 @@
+
+const Reader = require("./../../../lib/utils/Reader")
+
+describe("Reader", () => {
+
+    test("Read for th json explorers", () => {
+        const explorers = Reader.readJsonFile("explorers.json")
+        expect(explorers.length).toBe(15)
+
+    })
+
+})
